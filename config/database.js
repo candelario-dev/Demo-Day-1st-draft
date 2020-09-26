179d421b4754afaf0476f65ee49a2f27754d6cc9
Rebecca Candelario <69063941+candelario-dev@users.noreply.github.com>
2020-09-26 17:40:26 -0400

    Unzipped original zip file

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://becca:becca@clusterdemo.rpiuh.mongodb.net/test', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
